eca07aba257812849e6bf48b810d1c9f44fea44e
Coymw1 <coymw1@ad.unc.edu>
2023-05-09 21:34:08 -0400

    update

--- server.js
+++ server.js
@@ -4,27 +4,29 @@ import { rps, rpsls } from './lib/rpsls.js';
 import express from 'express';
 import minimist from 'minimist';
 
+
+const port = argv.port || 5000;
+
 var argv = minimist(process.argv.slice(2));
 var app = express();
-const port = argv.port || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
 //just returns 200 OK
 app.get('/app', (req, res)=>{
     res.status(200).send("200 OK");
 });
 
-
-
 //endpoint for rps
 app.get('/app/rps', (req, res) =>{
-    res.status(200).send(JSON.stringify(rps()));
+    res.status(200).send((rps()));
 });
 
 //endpoint for rpsls
 app.get('/app/rpsls', (req, res) =>{
-    res.status(200).send(JSON.stringify(rpsls()));
+    res.status(200).send((rpsls()));
 });
 
 //rps with user input
@@ -47,14 +49,14 @@ app.post('/app/rpsls/play/', (req, res)=>{
     res.status(200).send(rpsls(req.body.shot));
 });
 
-//rps wiht data from url
+//rps with data from url
 app.get('/app/rps/play/:shot/', (req, res)=>{
-    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+    res.status(200).send((rps(req.params.shot)));
 });
 
 //rpsls with data from URL
 app.get('/app/rpsls/play/:shot/', (req, res)=>{
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+    res.status(200).send((rpsls(req.params.shot)));
 });
 
 //404 for undefined endpoints
